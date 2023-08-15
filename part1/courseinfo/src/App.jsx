b1b65b6b9005037ad60d3c4df44cff8bde1b3b13
Toby Flanagan <sc21tf@leeds.ac.uk>
2023-08-15 13:24:32 +0100

    exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -49,8 +49,9 @@ const Total = (props) => {
 
 //main component
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10
@@ -64,13 +65,14 @@ const App = () => {
         exercises: 14
       }
     ]
+  }
   return (
     <div>
-      <Header course={course}/>
+      <Header course={course.name}/>
       
-      <Content parts={parts}/>
+      <Content parts={course.parts}/>
 
-      <Total parts={parts}/>
+      <Total parts={course.parts}/>
 
     </div>
   )
