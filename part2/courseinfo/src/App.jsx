a04300a84cd1d8f36a4f951a1ecc980ea6d9098e
Toby Flanagan <sc21tf@leeds.ac.uk>
2023-08-22 13:36:03 +0100

    exc 2.5 complete

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,6 @@
 
 
-const Course = ({ course }) => {
-
-  return(
-    <>
-      <Header course= { course.name }/>
-      <Content parts = { course.parts }/>
-      <Total sum={course.parts.reduce((prev, curr) => prev+curr.exercises, 0)}/>
-    </>
-  )
-
-}
-
-
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => {
-
-  return(
-  <>
-    {parts.map(part=>
-      <Part key={part.id}
-        part={part}
-      />
-    )} 
-  </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
