0ed0bc9493c6d7564c5cce40b854c4ac947af66c
TobyFlan <toby.flanagan@hotmail.co.uk>
2023-08-30 13:59:10 +0100

    working filter input and fetch country names

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+//component used for changing/inputting new search queries
+const SearchBar = ({setNewFilter}) => {
+
+  const [value, setValue] = useState('')
+
+  const handleInput = (event) => {
+    console.log(`the box has become: ${event.target.value}`)
+    setValue(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+    console.log(`user has searched for ${value.toLowerCase()}`)
+    setNewFilter(value.toLowerCase())
+  }
+
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <form  onSubmit={handleSearch}>
+        find new countries: 
+        <input value={value} onChange={handleInput}></input>
+        <button type={'submit'}>find</button>
+      </form>
     </>
   )
+
+}
+
+
+//component to read the filter and gather results
+const DisplayResults = (props) => {
+
+  return null
+
+}
+
+
+function App() {
+
+  const [newFilter, setNewFilter] = useState('')
+
+
+  //test getting all api results
+  const [allNames, setAllNames] = useState([])
+
+
+  //useEffect to get all names from rest API only once 
+  useEffect(() => {
+
+    axios
+    .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+    .then(response => {
+      setAllNames(response.data.map(n => n.name.common))
+    })
+
+  }, [])
+
+
+
+  return (
+    <div>
+      <SearchBar setNewFilter={setNewFilter}/>
+    </div>
+  )
+
 }
 
 export default App
